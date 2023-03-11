b59410b178958e4f4eafe6368a304158813ac00d
Carlota <carlota.monederoh@gmail.com>
2023-03-11 13:43:45 +0100

    Modified print to 1000 regs

--- flights.py
+++ flights.py
@@ -1,7 +1,6 @@
 # /opt/miniconda3/bin/conda init
 # conda activate nosql_mbd25
 #!pip install happybase
-
 import happybase
 import csv
 def getConn():
@@ -19,7 +18,7 @@ flights_2008 = '/tmp/nosql/airData/2008.csv'
 flights_data = [flights_2007, flights_2008]
 
 for flight_path in flights_data:
-    print("loading %s into airports" % flight_path)
+    print("loading %s into flights" % flight_path)
     with open(flight_path, 'r') as f:
         reader = csv.DictReader(f)
         i = 0
@@ -32,8 +31,6 @@ for flight_path in flights_data:
                 row['DayofMonth'] = str(0)+row['DayofMonth']
 
             rowkey = row['Year']+row['Month']+row['DayofMonth']+row['FlightNum']+row['Origin']+row['Dest']
-            # hora de salida, hora de llegada, número de vuelo, origen, destino, número de aeronave y distancia recorrida.
-            i += 1
             flights.put(to_bytes(rowkey),
                 {
                 to_bytes('info:DepTime'): to_bytes(row['DepTime']),
@@ -43,6 +40,12 @@ for flight_path in flights_data:
                 to_bytes('info:Dest'): to_bytes(row['Dest']),
                 to_bytes('info:TailNum'): to_bytes(row['TailNum']),
                 to_bytes('info:Distance'): to_bytes(row['Distance'])
-        })
-            
-    print("loaded airports with %d entries" % i)
+            })
+
+            i += 1
+
+            # keep track of execution evolution in console
+            if i % 1000 == 0:
+                print("Loaded %d entries" % i)
+        
+    print("loaded {} with {} entries".format(flight_path, i)) 
