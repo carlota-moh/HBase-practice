fbbae55e60bbd44c672de51e944fc6d020890feb
Jorge Ayuso Martínez <jorgeayusomartinez@gmail.com>
2023-03-17 19:15:46 +0100

    Add new aggregates (not commented everything yet)

---
+++ src/3.2-aggregates.py
@@ -0,0 +1,250 @@
+import os
+import pandas as pd
+import happybase
+
+# Establish connection with HBase
+def getConn(ip):
+    """
+    Establish a connection with HBase using happybase library.
+
+    Args:
+    ----------
+    ip : str
+        ip address a connection needs to be established with.
+
+    Returns:
+    ----------
+        Connection with HBase cluster in the IP address specified.
+    """
+    return happybase.Connection(host=ip)
+
+# Function to convert to bytes the data which will be stored in HBase
+def to_bytes(value):
+    """
+    Given a specific value, returns it converted 
+    to bytes using UTF-8 encoding.
+
+    Args:
+    ----------
+    value : any
+        Any Python object which must be converted to bytes.
+
+    Returns:
+    ----------
+        Value introduced as input, but converted to bytes.
+    """
+    return bytes(str(value), "utf-8")
+
+# Generate column containing row key
+def generate_rowkey(df, new_col, col1, col2):
+    """
+    Given a DataFrame, generates a new column by combining other two columns.
+
+    Args:
+    ----------
+    df : pd.DataFrame
+        DataFrame in which a new column wants to be created.
+    new_col : str
+        New column which will be created.
+    col1 : str
+        First column to be combined.
+    col2 : str
+        Second column to be combined.
+
+    Returns:
+    ----------
+        Input DataFrame, but having added the new column specified.
+    """
+    df[new_col] = df[col1] + df[col2]
+    return df
+
+# Calculate average air time by route
+def calculate_avg_duration(df):
+    """
+    Given a DataFrame, groups by the rowKey column and calculates the 
+    average flight duration for each group.
+
+    Args:
+    ----------
+    df : pd.DataFrame
+        Input DataFrame.
+
+    Returns:
+    ----------
+        Grouped DataFrame.
+    """
+    df_avg_duration = df.groupby(by="rowKey").agg(Avg_duration=("AirTime", "mean")).reset_index()
+    return df_avg_duration
+
+# Calculate average delay for each airline and route
+def calculate_avg_delay(df):
+    """
+    Given a DataFrame, groups by the rowKey and UniqueCarrier columns and calculates the 
+    average departure and arrival delay for each group.
+
+    Args:
+    ----------
+    df : pd.DataFrame
+        Input DataFrame.
+
+    Returns:
+    ----------
+        Grouped DataFrame.
+    """
+    df_avg_delay = df.groupby(by=["rowKey", "UniqueCarrier"]).agg(
+        Avg_delay_dep=("DepDelay", "mean"), 
+        Avg_delay_arr=("ArrDelay", "mean")
+        ).reset_index()
+    return df_avg_delay
+
+# Get airlines with higher number of routes per route
+def sort_by_freq(df):
+    """
+    Given a DataFrame, groups by the rowKey and UniqueCarrier columns and calculates the 
+    number of flights for each group. It also sorts the information by rowKey and the number 
+    of flights in ascending and descending order respectively.
+
+    Args:
+    ----------
+    df : pd.DataFrame
+        Input DataFrame.
+
+    Returns:
+    ----------
+        Grouped DataFrame.
+    """
+    df_n_routes = df.groupby(by=["rowKey", "UniqueCarrier"]).agg(
+        N_routes=("UniqueCarrier", "count")
+        ).reset_index()
+    df_n_routes.sort_values(by=["rowKey", "N_routes"], ascending=[True, False], inplace=True)
+    return df_n_routes
+
+# Get most used airplane by UniqueCarrier
+def get_most_used_airplane(df):
+    """
+    Given a DataFrame, groups by the rowKey, UniqueCarrier and TailNum columns and calculates the 
+    number of flights for each group. It also sorts the information by rowKey, UniqueCarrier and number 
+    of flights in ascending, ascending and descending order respectively.
+
+    Args:
+    ----------
+    df : pd.DataFrame
+        Input DataFrame.
+
+    Returns:
+    ----------
+        Grouped DataFrame.
+    """
+    df_airplane = df.groupby(by=["rowKey", "UniqueCarrier", "TailNum"]).agg(
+        N_routes_airplane=("UniqueCarrier", "count")
+        ).reset_index()
+    df_airplane.sort_values(by=["rowKey", "UniqueCarrier", "N_routes_airplane"], ascending=[True, True, False], inplace=True)
+    df_airplane = df_airplane.groupby(by=["rowKey", "UniqueCarrier"]).first().reset_index()
+    return df_airplane
+
+def format_number(number, n_digits=5):
+    len_number = len(str(number))
+    n = "0"*(n_digits-len_number) + str(number)
+    return n
+
+def insert_df(table, df):
+    for row in df.itertuples():
+        # Get number with binaries
+        n_routes = format_number(row.N_routes)
+        column_carrier = n_routes
+        # column_carrier = row.UniqueCarrier
+        column_global = "99999"
+        table.put(
+            to_bytes(row.rowKey), 
+                {
+                    to_bytes(f"C:{column_carrier}"): to_bytes(row.JSON),
+                    to_bytes(f"C:{column_global}"): to_bytes(row.Route_avg_duration)
+                    }
+                )
+
+def load_df_to_hbase(table, df):
+    count = 0
+    airlines_info = []
+    routes_avg_duration = []
+    for row in df.itertuples():
+        airline_info = {
+            "UniqueCarrier": row.UniqueCarrier,
+            "AvgDelayDep": row.Avg_delay_dep,
+            "AvgDelayArr": row.Avg_delay_arr,
+            # "Nroutes": row.N_routes,
+            "TailNum": row.TailNum,
+            "NroutesAirplane": row.N_routes_airplane 
+        }
+
+        route_avg_duration = {
+            "Route_avg_duration": row.Avg_duration
+        }
+
+        airlines_info.append(airline_info)
+        routes_avg_duration.append(route_avg_duration)
+
+    df["JSON"] = airlines_info
+    df["Route_avg_duration"] = routes_avg_duration
+
+    # Console message
+    count += len(df)
+    if (count % 10**6) == 0:
+        print(f"{count}")
+
+    # Insert data into HBase
+    insert_df(table=table, df=df)
+
+
+
+
+# Files with routes data
+data_dir = os.path.join("/tmp", "nosql", "airData")
+years = ["2007", "2008"]
+file_format = ".csv"
+
+# IP address of server where HBase is configured
+ip = "192.168.80.33"
+# Namespace in HBase
+namespace = "airdata_425"
+# Table name in HBase
+table = "routes"
+
+columns = [
+    "Origin",
+    "Dest",
+    "AirTime",
+    "UniqueCarrier",
+    "DepDelay",
+    "ArrDelay",
+    "TailNum"
+]
+
+###########################################################################
+
+# Get connection with specified namespace and table
+routes = getConn(ip).table(f"{namespace}:{table}")
+
+data = pd.DataFrame(columns=columns)
+
+for year in years:
+    filepath = os.path.join(data_dir, year + file_format)
+    # Get aggregates
+    data_year = pd.read_csv(filepath, sep=',', usecols=columns)
+    data = pd.concat([data, data_year], axis=0)
+
+df = generate_rowkey(data, "rowKey", "Origin", "Dest")
+df_avg_duration = calculate_avg_duration(df)
+df_avg_delay = calculate_avg_delay(df)
+df_n_routes = sort_by_freq(df)
+df_airplane = get_most_used_airplane(df)
+
+# JOINS
+df_final = df_avg_delay.merge(df_avg_duration, how="left", on="rowKey")
+df_final = df_final.merge(df_n_routes, how="left", on=["rowKey", "UniqueCarrier"])
+# This df has different number of rows (2 less), and I do not know why yet 
+df_final = df_final.merge(df_airplane, how="left", on=["rowKey", "UniqueCarrier"])
+# Sort by N_route
+df_final.sort_values(by=["rowKey", "N_routes"], ascending=[True, False], inplace=True)
+print(df_final.shape)
+print(df_final.head())
+# load_df_to_hbase(table=routes, df=df_final)
