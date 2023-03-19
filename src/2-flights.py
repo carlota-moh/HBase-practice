7e301982a2e2ff7609ed04451b43466b0515b025
Carlota <carlota.monederoh@gmail.com>
2023-03-19 11:15:20 +0100

    Renamed flights_by_column.py to flights.py

--- src/2-flights.py
+++ src/2-flights.py
@@ -1,73 +1,169 @@
-# /opt/miniconda3/bin/conda init
-# conda activate nosql_mbd25
-# !pip install happybase
-
-# ======================================================================================================================================
-# INSERTING DATA IN BATCHES INSTEAD OF GOING ONE BY ONE
-# ======================================================================================================================================
-
+import os
 import happybase
-import csv
+import pandas as pd
+import numpy as np
 
-def getConn():
-    return happybase.Connection(host='192.168.80.33')
+# Establish connection with HBase
+def getConn(ip):
+    """
+    Establish a connection with HBase using happybase library.
+
+    Args:
+    ----------
+    ip : str
+        ip address a connection needs to be established with.
+
+    Returns:
+    ----------
+        Connection with HBase cluster in the IP address specified.
+    """
+    return happybase.Connection(host=ip)
 
 # Function to convert to bytes the data which will be stored in HBase
 def to_bytes(value):
-  return bytes(str(value), 'utf-8')
+    """
+    Given a specific value, returns it converted 
+    to bytes using UTF-8 encoding.
+
+    Args:
+    ----------
+    value : any
+        Any Python object which must be converted to bytes.
+
+    Returns:
+    ----------
+        Value introduced as input, but converted to bytes.
+    """
+    return bytes(str(value), "utf-8")
+
+# Inserts information into HBase
+def insert_batch(table, df, batch_size):
+    """
+    Inserts information from a DataFrame into an HBase table.
+
+    Args:
+    ----------
+    table : 
+        HBase table table where the information needs to be inserted.
+    df: pd.DataFrame
+        DataFrame which contains the information to be inserted into HBase.
+    batch_size: int
+        Number of rows that should be inserted into the HBase table at once as a batch operation.
+
+    Returns:
+    ----------
+        None
+    """
+    with table.batch(batch_size=batch_size) as batch:
+        for row in df.itertuples():
+            column_name = row.UniqueCarrier + str(row.FlightNum) + row.Origin + row.Dest
+            batch.put(
+                to_bytes(row.rowkey), 
+                    {
+                        to_bytes(f"F:{column_name}"): to_bytes(row.JSON),
+                        }
+                    )
+
+# Create DataFrame with data formatted properly to be inserted into HBase
+def load_csv_to_hbase(table, filepath, columns, batch_size):
+    """
+    Loads CSV in batches as a DataFrame. Then creates two new columns (Year and JSON), 
+    which will be used to insert information into an HBase table.
+
+    Args:
+    ----------
+    table : 
+        HBase table where the information needs to be inserted.
+    filepath: str
+        filepath where the CSV file is stored.
+    columns: list(str)
+        List of columns to load in df.
+    batch_size: int
+        Number of rows that should be inserted into the HBase table at once as a batch operation.
+
+    Returns:
+    ----------
+        None
+    """
+    # Starts at 0
+    count = 0
+    # Get iterator
+    df_iterator = pd.read_csv(filepath, usecols=columns, chunksize=batch_size)
+    for df in df_iterator:
+        # Columns formatting
+        for col in ["Month", "DayofMonth"]:
+            df[col] = df[col].astype(str)
+            df[col] = np.where(len(df[col])==1, str(0)+df[col], df[col])
+        
+        # Create column for RowKey
+        df['Year'] = df['Year'].astype(str)
+        df["rowkey"] = df['Year'] + df['Month'] + df['DayofMonth']
+
+        # Create column in JSON format with all the required information
+        json_list = []
+        for row in df.itertuples():
+            json = {
+                "DepTime": row.DepTime,
+                "ArrTime": row.ArrTime,
+                "FlightNum": row.FlightNum,
+                "Origin": row.Origin,
+                "Dest": row.Dest,
+                "TailNum": row.TailNum,
+                "Distance": row.Distance 
+            }
+            json_list.append(json)
+        df["JSON"] = json_list
+
+        # Console message per n rows iterated
+        count += len(df)
+        if (count % 10**6) == 0:
+            print(f"{count}")
+
+        # Insert data into HBase
+        insert_batch(table=table, df=df, batch_size=batch_size)
+
+###################################################################################################
+# DEFINE VARIABLES
+###################################################################################################
+
+# IP address of server where HBase is configured
+ip = "192.168.80.33"
+# Namespace in HBase
+namespace = "airdata_425"
+# Table name in HBase
+table = "flights"
 
 # Get connection with specified namespace and table
-flights = getConn().table('airdata_425:flights')
+flights = getConn(ip).table(f"{namespace}:{table}")
 
-# Ficheros con datos sobre vuelos
-flights_2007 = '/tmp/nosql/airData/2007.csv'
-flights_2008 = '/tmp/nosql/airData/2008.csv'
+# Files with flights data
+data_dir = os.path.join("/tmp", "nosql", "airData")
+years = ["2007", "2008"]
+file_format = ".csv"
 
+# Get batches of size specified
 batch_size = 100000
 
-# Function to insert a batch of rows into HBase
-def insert_batch(table, rows):
-    with table.batch(batch_size=batch_size) as batch:
-        for row in rows:
-            rowkey = row['Year'] + row['Month'] + row['DayofMonth'] + row['UniqueCarrier'] + row['FlightNum'] + row['Origin'] + row['Dest']
-            batch.put(to_bytes(rowkey), {
-                to_bytes('info:DepTime'): to_bytes(row['DepTime']),
-                to_bytes('info:ArrTime'): to_bytes(row['ArrTime']),
-                to_bytes('info:FlightNum'): to_bytes(row['FlightNum']),
-                to_bytes('info:Origin'): to_bytes(row['Origin']),
-                to_bytes('info:Dest'): to_bytes(row['Dest']),
-                to_bytes('info:TailNum'): to_bytes(row['TailNum']),
-                to_bytes('info:Distance'): to_bytes(row['Distance'])
-            })
+# Columns of interest
+columns = [
+    "Year",
+    "Month",
+    "DayofMonth",
+    "UniqueCarrier",
+    "FlightNum",
+    "Origin",
+    "Dest",
+    "DepTime",
+    "ArrTime",
+    "TailNum",
+    "Distance"
+    ]
 
-# Function to load a CSV file into HBase using multithreading
-def load_csv_to_hbase(filename):
-    print("Loading %s into flights" % filename)
-    with open(filename, 'r') as f:
-        reader = csv.DictReader(f)
-        rows = []
-        i = 0
-        for row in reader:
-            # All months must have length 2
-            if len(row['Month']) == 1:
-                row['Month'] = str(0) + row['Month']
-            # All days must have length 2
-            if len(row['DayofMonth']) == 1:
-                row['DayofMonth'] = str(0) + row['DayofMonth']
-            rows.append(row)
-            i += 1
-            # Insert rows in batches
-            if i % batch_size == 0:
-                insert_batch(flights, rows)
-                rows = []
-                print("Loaded %d entries" % i)
+###################################################################################################
+# LOAD CSV FILES AND INSERT DATA INTO HBASE
+###################################################################################################
 
-        if rows:
-            insert_batch(flights, rows)
-            print("Loaded %d entries" % i)
-
-    print("Loaded {} with {} entries".format(filename, i))
-
-# load CSVs
-load_csv_to_hbase(flights_2007)
-load_csv_to_hbase(flights_2008)
+# Iterate through each file and insert information into HBase
+for year in years:
+    filepath = os.path.join(data_dir, year + file_format)
+    load_csv_to_hbase(table=flights, filepath=filepath, columns=columns, batch_size=batch_size)
